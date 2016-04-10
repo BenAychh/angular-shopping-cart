ba34747351b05e757769c8635b9c3dc8a33b3c75
BenAychh <ben.tomas.hernandez@gmail.com>
2016-04-09 21:30:52 -0600

    forgot to add contact and about

--- src/js/routes.js
+++ src/js/routes.js
@@ -1,15 +1,12 @@
 app.config(function ($routeProvider) {
   $routeProvider.
-
-    // when('/about', {
-    //   templateUrl: 'partials/about.html',
-    //   controller: 'aboutControl',
-    // })
-    // .when('/contact', {
-    //   templateUrl: 'partials/contact.html',
-    //   controller: 'contactControl',
-    // })
-    when('/cart', {
+    when('/about', {
+      templateUrl: 'partials/about.html',
+    })
+    .when('/contact', {
+      templateUrl: 'partials/contact.html',
+    })
+    .when('/cart', {
       templateUrl: 'partials/cart.html',
       controller: 'cartControl',
     })
